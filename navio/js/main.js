85c18bedb837cfc7647b910a192a14f6fa7103f6
Fabian Peña <fabiancpl@ieee.org>
2019-02-08 07:12:59 -0500

    Navio and tsne integrated

--- navio/js/main.js
+++ navio/js/main.js
@@ -1,9 +1,17 @@
 
+const proj_width = 500,
+  proj_height = 400,
+  proj_padding = 20
+  proj_r = 3;
+
+var proj_svg, proj_g, 
+  proj_xScale, proj_yScale, 
+  proj_colorScale;
+
 var data,
   visibleData,
-  features;
-  //catFeatures,
-  //seqFeatures;
+  features,
+  proj_features;
 
 var nv,
   nv_height = 500;
@@ -34,8 +42,79 @@ function drag( event ) {
 function drop( event ) {
   event.preventDefault();
   var elementId = event.dataTransfer.getData( 'text' );
-  if( event.target.tagName !== 'BUTTON' )
+  console.log( event.target.tagName );
+  if( event.target.tagName === 'DIV' )
     event.target.appendChild( document.getElementById( elementId ) );
+}
+
+// Define the div for the tooltip
+var featureGlyph = d3.select( 'body' )
+  .append( 'div' ) 
+    .attr( 'class', 'feature-glyph' )       
+    .style( 'opacity', 0 );
+
+function buildFeatureGlyph( feature ) {
+
+  console.log( feature );
+
+  featureGlyph
+    .html( '' )
+    .style( 'left', ( d3.event.pageX + 20 ) + 'px' )   
+    .style( 'top', ( d3.event.pageY + 20 ) + 'px' );
+
+  data_sym = [
+    {
+      "x" : "a",
+      "y" : 5,
+    },
+    {
+      "x" : "b",
+      "y" : 10,
+    },
+    {
+      "x" : "c",
+      "y" : 15,
+    },
+  ];
+
+  var svgGlyph = featureGlyph.append( 'svg' )
+    .attr( 'width', 140 )
+    .attr( 'height', 140 );
+
+    var g = svgGlyph.append("g")
+      .attr( 'transform', 'translate(5,5)' );
+
+  var xScale = d3.scaleBand().rangeRound( [ 0, 140 ] ).padding( 0.1 ),
+    yScale = d3.scaleLinear().rangeRound( [ 140, 0 ] );
+  
+  xScale.domain( data_sym.map( function( d ) { return d.x; }));
+  yScale.domain( [ 0, d3.max( data_sym, function( d ) { return d.y; })]);
+
+  g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + 140 + ")")
+            .call(d3.axisBottom(xScale));
+
+        g.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(yScale))
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Frequency");
+
+g.selectAll(".bar")
+          .data(data_sym)
+          .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return xScale(d.x); })
+            .attr("y", function(d) { return yScale(d.y); })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) { return 140 - yScale(d.y); });
+
+
 }
 
 function initFeatureSelection() {
@@ -46,10 +125,24 @@ function initFeatureSelection() {
     .enter()
       .append( 'button' )
         .attr( 'id', d => d.name )
-        .attr( 'class', d => 'btn ' + ( ( d.type === 'categorical' ) ? 'btn-categorical' : 'btn-primary' ) )
+        .attr( 'class', d => 'btn btn-secondary' )
         .attr( 'draggable', true )
-        .attr( 'ondragstart', 'drag(event)' );
-  
+        .attr( 'ondragstart', 'drag(event)' )
+        .on( 'mouseover', ( feature ) => {
+
+          featureGlyph.transition()    
+            .duration( 500 )    
+            .style( 'opacity', 1 );
+
+          buildFeatureGlyph( feature );          
+              
+        } )          
+        .on( 'mouseout', () => {   
+          featureGlyph.transition()    
+            .duration( 500 )    
+            .style( 'opacity', 0 ); 
+        } );
+
   featButtons
     .append( 'input' )
       .attr( 'id', d => d.name + '-chk' )
@@ -64,7 +157,7 @@ function initFeatureSelection() {
 
         // Update feature visualization
         d3.select( '#' + feature.name )
-          .attr( 'class', d => 'btn ' + ( ( chk === false ) ? 'btn-secondary' : ( ( d.type === 'categorical' ) ? 'btn-categorical' : 'btn-primary' ) ) )
+          .attr( 'class', d => 'btn ' + ( ( chk === false ) ? 'btn-light' : 'btn-secondary' ) )
           .property( 'disabled', !chk );
 
         //TODO: Update Navio
@@ -77,59 +170,161 @@ function initFeatureSelection() {
 
   featButtons
     .append( 'i' )
-    .attr( 'class', 'fas fa-cog' );
+    .attr( 'id', d => d.name + '-type' )
+    .attr( 'class', 'text-warning' )
+    .html( d => ( ( d.type === 'categorical' ) ? '<b>A</b>' : '<b>#</b>' ) );
+    /*.on( 'click', ( feature ) => {
+      
+      // TODO: Change type in original dataset
+
+      // Modifying feature property
+      if( feature.type === 'categorical' ) {
+        feature.type = 'sequential';
+      } else {
+        feature.type = 'categorical';
+      }
+
+      // Modifying feature visual aspect in list
+      d3.select( '#' + feature.name + '-type' )
+        .html( d => ( ( d.type === 'categorical' ) ? '<b>A</b>' : '<b>#</b>' ) );
+    } );*/
 
 }
 
 function createFeatureGroup() {
 
-  if( $('#group-name').val() !== '' ) {
-    console.log( 'New feature group: ' + $('#group-name').val() );
+  if( $( '#group-name' ).val() !== '' ) {
+    console.log( 'New feature group: ' + $( '#group-name' ).val() );
 
-    var groupName = $('#group-name').val();
+    var groupName = $( '#group-name' ).val();
     var groupId = groupName.toLowerCase().replace( ' ', '-' );
 
     // Create DOM element
     var featureGroup = d3.select( '#feature-selection' ).append( 'div' )
-        .attr( 'id', groupId )
-        .attr( 'class', 'feature-group' )
-        .attr( 'ondrop', 'drop(event)' )
-        .attr( 'ondragover', 'allowDrop(event)' );
+      .attr( 'id', groupId )
+      .attr( 'class', 'feature-group' )
+      .attr( 'ondrop', 'drop(event)' )
+      .attr( 'ondragover', 'allowDrop(event)' );
 
     featureGroup.append( 'div' )
       .attr( 'class', 'header' )
       .html( groupName );
 
-    $('#newFeatureGroup').modal( 'hide' );
+    $( '#newFeatureGroup' ).modal( 'hide' );
   }
 
 }
 
-d3.csv( './data/animals.csv' ).then( function( data ) {
+function loadData( url ) {
 
-  this.data = data;
-  visibleData = data;
+  //d3.csv( './data/animals.csv' ).then( function( data ) {
+  d3.csv( url ).then( function( data ) {
 
-  // Get features from data and identify type
-  // TODO: Method for select feature type
-  features = []
-  keys = Object.keys( data[ 0 ] );
-  keys.forEach( ( k ) => {
-    type = 'categorical';
-    if( k === 'legs' ) {
-      type = 'sequential';
-    }
-    features.push( { 'name' : k, 'type' : type, 'visible' : true } );
-  } )
+    this.data = data;
+    visibleData = data;
 
-  
-  // Create feature selection elements
-  initFeatureSelection();
+    // Get features from data and identify type
+    // TODO: Method for select feature type
+    features = []
+    keys = Object.keys( data[ 0 ] );
+    keys.forEach( ( k ) => {
+      type = 'categorical';
+      if( k === 'legs' ) {
+        type = 'sequential';
+      }
+      features.push( { 'name' : k, 'type' : type, 'visible' : true } );
+    } )
+    
+    // Create feature selection elements
+    initFeatureSelection();
 
-  // Initialize and put data in Navio
-  initNavio();
-  nv.data( this.data );
+    // Initialize and put data in Navio
+    initNavio();
+    nv.data( this.data );
 
-  nv.updateCallback( () => visibleData = nv.getVisible() );
+    nv.updateCallback( () => visibleData = nv.getVisible() );
 
-} );
+    // Initialize projection panel
+    initProjection()
+
+    proj_features = [ 'hair', 'feathers', 'eggs', 'milk', 'airborne', 'aquatic', 'predator', 'toothed', 'backbone', 'breathes', 'venomous', 'fins', 'legs', 'tail', 'domestic', 'catsize' ];
+
+    projection = project( data );
+    
+    projection.forEach( ( d, i ) => {
+      data[ i ][ 'x' ] = d[ 0 ];
+      data[ i ][ 'y' ] = d[ 1 ];
+    } );
+
+    drawProjection();
+
+  } );
+
+}
+
+function initProjection() {
+
+  d3.select( '#projection' ).html( '' );
+
+  // Define projection scales
+  proj_xScale = d3.scaleLinear()
+    .range( [ proj_padding, proj_width - proj_padding ] );
+    
+  proj_yScale = d3.scaleLinear()
+    .range( [ proj_height - proj_padding, proj_padding ] );
+
+  // Define color scale
+  proj_colorScale = d3.scaleOrdinal( d3.schemeCategory10 )
+    .domain( d3.map( data, d => d.class ) );
+
+  // Create SVG
+  proj_svg = d3.select( '#projection' )
+    .append( 'svg' )
+      .attr( 'width', proj_width )
+      .attr( 'height', proj_height )
+
+  proj_g = proj_svg.append( 'g' );
+
+}
+
+function drawProjection() {
+
+  proj_xScale
+    .domain( [ d3.min( data, d => d.x ), d3.max( data, d => d.x ) ] );
+    
+  proj_yScale
+    .domain( [ d3.min( data, d => d.y ), d3.max( data, d => d.y ) ] );
+
+  proj_svg.append( 'text' )
+    .attr( 'x', proj_padding )
+    .attr( 'y', proj_padding )
+    .text( 'Iterations: ' + ITERATIONS + '\nPerplexity: ' + PERPLEXITY );
+
+  proj_g.selectAll( 'circle' )
+    .data( data )
+    .enter()
+      .append( 'circle' )
+      .attr( 'cx', d => proj_xScale( d.x ) )
+      .attr( 'cy', d => proj_yScale( d.y ) )
+      .attr( 'r', proj_r )
+      .attr( 'fill', d => proj_colorScale( d.class ) );
+
+}
+
+// Event handler to load data by user
+d3.select( '#file-input' )
+  .on( 'change', function() {
+
+    var file = d3.event.target.files[ 0 ];
+    if ( !file ) return;
+    d3.select( '#file-name' ).html( file.name );
+    console.log( file.name );
+
+    var reader = new FileReader();
+    reader.onloadend = function( evt ) {
+      var dataUrl = evt.target.result;
+      loadData( dataUrl );
+    };
+    reader.readAsDataURL( file );
+
+  } );
---
+++ navio/js/project.js
@@ -0,0 +1,41 @@
+
+/* Hyper-parameters algorithm */
+var ITERATIONS = 1000,
+  PERPLEXITY = 30,
+  EPSILON = 200
+  DIMENSIONS = 2;
+
+function project( data ) {
+
+  var arrayData = [];
+
+  // Transform data to multi-dimensional array
+  data.forEach( d => {
+    
+    row = [];
+    proj_features.forEach( k => {
+      row.push( +d[ k ] );
+    } );
+    arrayData.push( row );
+  } );
+
+  // Build hyper-parameters object
+  var params = {}
+  params.epsilon = EPSILON; 
+  params.perplexity = PERPLEXITY;
+  params.dim = DIMENSIONS;
+
+  console.info( 'Projecting data in ' + ITERATIONS + ' iterations!' );
+
+  // Instantiate t-SNE object and attach the data
+  var tsne = new tsnejs.tSNE( params );
+  tsne.initDataRaw( arrayData );
+
+  // Run t-SNE iterations
+  for( var k = 0; k < ITERATIONS; k++ ) {
+    tsne.step();
+  }
+
+  return tsne.getSolution();
+
+}
